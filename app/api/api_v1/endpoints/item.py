3431324c20c0fde3d2e50e5f54d2858ce998471a
ooojay <pjy101102@gmail.com>
2023-06-11 18:02:25 +0900

    Add item get, create, delete

--- app/api/api_v1/endpoints/item.py
+++ app/api/api_v1/endpoints/item.py
@@ -1,19 +1,51 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.api import deps
-from app.schemas.item import ItemBase, ItemCreate, ItemCreateResponse
+from app.schemas.item import ItemCreate, ItemInDBBase
 
 router = APIRouter()
 
 
-@router.post("/create/", status_code=201, response_model=ItemCreateResponse)
+@router.get("/{item_id}", status_code=200, response_model=ItemInDBBase)
+def fetch_item(*, item_id: str, db: Session = Depends(deps.get_db)) -> ItemInDBBase:
+    """
+    Get an item in the database. search by item_id.
+    """
+    result = crud.item.get(db=db, id=item_id)
+    if not result:
+        raise HTTPException(
+            status_code=404, detail=f"Item with ID {item_id} not exists"
+        )
+
+    return ItemInDBBase.from_orm(result)
+
+
+@router.post("/create", status_code=201, response_model=ItemInDBBase)
 def create_item(*, item_in: ItemCreate,
-                db: Session = Depends(deps.get_db)) -> dict:
+                db: Session = Depends(deps.get_db)) -> ItemInDBBase:
     """
     Create a new item in the database.
     """
-    recipe = crud.item.create(db=db, obj_in=item_in)
+    if crud.item.get(db, item_in.id):
+        raise HTTPException(
+            status_code=404, detail=f"Item with ID {item_in.id} already exists"
+        )
 
-    return recipe
+    result = crud.item.create(db=db, obj_in=item_in)
+
+    return ItemInDBBase.from_orm(result)
+
+
+@router.delete("/delete/{item_id}", status_code=200, response_model=ItemInDBBase)
+def delete_item(*, item_id: str, db: Session = Depends(deps.get_db)) -> ItemInDBBase:
+    """
+    Delete an item in the database. search by item_id.
+    """
+    if not crud.item.get(db, item_id):
+        raise HTTPException(status_code=404, detail=f"Item not found with item_id {item_id}")
+
+    result = crud.item.remove(db=db, id=item_id)
+
+    return ItemInDBBase.from_orm(result)
--- app/crud/crud_item.py
+++ app/crud/crud_item.py
@@ -5,23 +5,11 @@ from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
 from app.models.item import Item
-from app.schemas.item import (ItemBase, ItemCreate, ItemCreateResponse,
+from app.schemas.item import (ItemBase, ItemCreate,
                               ItemUpdate)
 
 
 class CRUDItem(CRUDBase[ItemBase, ItemCreate, ItemUpdate]):
-
-    def get_by_item_id(self, db: Session, *, item_id: str) -> Optional[Item]:
-        return db.query(Item).filter(Item.item_id == item_id).first()
-
-    def create(self, db: Session, *, obj_in: ItemCreate) -> ItemCreateResponse:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)  # type: ignore
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-
-        return ItemCreateResponse.from_orm(db_obj)
 
     def update(self, db: Session, *, db_obj: Item, obj_in: Union[ItemUpdate,
                                                                  Dict[str, Any]]) -> Item:
