6478d85bc99675f5b84247f14c71af33a4825d02
ooojay <pjy101102@gmail.com>
2023-06-11 18:07:31 +0900

    Add ItemUpdate schema

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from pydantic import BaseModel
-from sqlalchemy import BLOB
 
 
 class ItemBase(BaseModel):
@@ -30,11 +29,27 @@ class ItemCreate(ItemBase):
         }
 
 
-class ItemUpdate(ItemBase):
-    quantity: int
+class ItemUpdate(BaseModel):
+    id: str
+    category: Optional[str]
+    quantity: Optional[int]
     callname: Optional[str]
     description: Optional[str]
-    image = BLOB
+    price: Optional[int]
+    image_url: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "DD1391-100",
+                "category": "Shoes",
+                "quantity": 2,
+                "callname": "덩크 로우 범고래",
+                "description": "스테디셀러",
+                "price": 119000,
+                "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhypebeast.kr%2F2022%2F3%2Fnike-dunk-low-black-official-release-info&psig=AOvVaw1ac7rjz-I-UwN6H8pasuuE&ust=1686558150092000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJCy8ezkuv8CFQAAAAAdAAAAABAE"
+            }
+        }
 
 
 # Properties shared by models stored in DB
