3d7c2a5ea2f2293b15073c2ad2095684bcead3ce
ooojay <pjy101102@gmail.com>
2023-06-11 17:33:30 +0900

    Update: Item model and schema

--- app/models/item.py
+++ app/models/item.py
@@ -9,3 +9,5 @@ class Item(Base):
     callname = Column(String(length=256), nullable=True)
     quantity = Column(Integer, nullable=False)
     description = Column(String(length=256), nullable=True)
+    price = Column(Integer, nullable=True)
+    image_url = Column(String(length=1024), nullable=True)
--- app/schemas/item.py
+++ app/schemas/item.py
@@ -5,7 +5,7 @@ from sqlalchemy import BLOB
 
 
 class ItemBase(BaseModel):
-    item_id: Optional[str]
+    item_id: str
     category: Optional[str]
     quantity: int = 0
 
@@ -13,6 +13,8 @@ class ItemBase(BaseModel):
 class ItemCreate(ItemBase):
     callname: Optional[str]
     description: Optional[str]
+    price: Optional[int]
+    image_url: Optional[str]
 
     class Config:
         schema_extra = {
@@ -21,7 +23,9 @@ class ItemCreate(ItemBase):
                 "category": "Shoes",
                 "quantity": 1,
                 "callname": "덩크 로우 범고래",
-                "description": "스테디셀러"
+                "description": "스테디셀러",
+                "price": 119000,
+                "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhypebeast.kr%2F2022%2F3%2Fnike-dunk-low-black-official-release-info&psig=AOvVaw1ac7rjz-I-UwN6H8pasuuE&ust=1686558150092000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJCy8ezkuv8CFQAAAAAdAAAAABAE"
             }
         }
 
@@ -36,3 +40,19 @@ class ItemUpdate(ItemBase):
     callname: Optional[str]
     description: Optional[str]
     image = BLOB
+
+
+# Properties shared by models stored in DB
+class ItemInDBBase(ItemBase):
+    callname: Optional[str]
+    description: Optional[str]
+    price: Optional[int]
+    image_url: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to return to client
+class Item(ItemInDBBase):
+    pass
