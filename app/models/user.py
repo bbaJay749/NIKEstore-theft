9b84f3555f7e18ad4d365a6e8509fbbd676e07f0
Onee <scwon3796@gmail.com>
2023-07-23 16:26:03 +0900

    ADD: user db schemas

--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,23 @@
-from sqlalchemy import Integer, String, Column, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy import String, Column, Boolean
+#from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
 
 class User(Base):
-    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String(20), primary_key=True, index=True)
+    pwd = Column(String(256), nullable=True)
     first_name = Column(String(256), nullable=True)
-    surname = Column(String(256), nullable=True)
+    last_name = Column(String(256), nullable=True)
     email = Column(String, index=True, nullable=False)
     is_superuser = Column(Boolean, default=False)
-    recipes = relationship(
+    
+    # wish_list는 relationship으로 연결 필요함
+    wish_list = Column(String(256), nullable=True)
+    '''recipes = relationship(
         "Recipe",
         cascade="all,delete-orphan",
         back_populates="submitter",
         uselist=True,
     )
+    '''
